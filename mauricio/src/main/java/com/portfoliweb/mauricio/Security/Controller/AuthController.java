950ba27c16ac9bf81201a72eb13ecc4832f73760
Mauricio <mauricio.mazurek7@gmail.com>
2022-09-23 13:10:27 -0300

    procfile

--- mauricio/src/main/java/com/portfoliweb/mauricio/Security/Controller/AuthController.java
+++ mauricio/src/main/java/com/portfoliweb/mauricio/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin ("https://portfolio-web-e9f81.web.app")
+@CrossOrigin 
 public class AuthController {
 
     @Autowired
